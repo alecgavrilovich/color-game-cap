4f524c0fad47a10a1be21db87f99b0eb1118222f
Alec Gavrilovich <alecgavrilovich@gmail.com>
2017-02-24 22:21:08 +0100

    part-8

--- colorGame.js
+++ colorGame.js
@@ -1,15 +1,67 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-];
-
+var numOfSquares = 6;
+var colors = generateRandomColors(numOfSquares);
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var modeButtons = document.querySelectorAll(".mode");
+
+for (var i = i; i modeButtons.length; i++) {
+   modeButtons.addEventListener("click", function(){
+       modeButtons[0].classList.remove("selected");
+       modeButtons[1].classList.remove("selected");
+       this.classList.add("selected");
+       
+   }) 
+};
+
+easyBtn.addEventListener("click", function(){
+    hardBtn.classList.remove("selected");
+    easyBtn.classList.add("selected");
+    numOfSquares = 3
+    colors = generateRandomColors(numOfSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (var i = 0; i < squares.length; i++) {
+        if(colors[i]) {
+            squares[i].style.background = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
+    }
+    
+});
+
+hardBtn.addEventListener("click", function(){
+    easyBtn.classList.remove("selected");
+    hardBtn.classList.add("selected");
+    numOfSquares = 6;
+    colors = generateRandomColors(numOfSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.background = colors[i];
+        squares[i].style.display = "block";
+    }
+});
+
+resetButton.addEventListener("click", function () {
+    //generate all new colors
+    colors = generateRandomColors(numOfSquares);
+    //pick a new random color from array
+    pickedColor = pickColor();
+    //change colors of squares
+    colorDisplay.textContent = pickedColor;
+    this.textContent = "New Colors";
+    messageDisplay.textContent = "";
+    //change colors of squares
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.background = colors[i]; 
+    }
+    h1.style.background = "steelblue";
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -23,10 +75,48 @@ for (var i = 0; i < squares.length; i++) {
         var clickedColor = this.style.background;
         //compare color to pickedColor
         if(clickedColor === pickedColor) {
-            alert("Correct");
+            messageDisplay.textContent = "Correct!";
+            resetButton.textContent = "Play Again?";
+            changeColors(clickedColor);
+            h1.style.background  = clickedColor;
         } else {
-            alert("Wrong!!!");
+            this.style.background = "#232323";
+            messageDisplay.textContent = "Try Again";
         }
     });
-    
+}
+
+function changeColors(color) {
+    //loot trough all squares
+    for (var i = 0; i < squares.length; i++) {
+        //change each color to match given color
+        squares[i].style.background = color;
+    }
+}
+
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
+
+function generateRandomColors(num) {
+    //make an array
+    var arr= [];
+    //add num random colors to arr
+    for (var i = 0; i < num; i++) {
+        //get Random color and push into arr
+        arr.push(randomColor());
+    }
+    //return that array
+    return arr;
+}
+
+function randomColor() {
+    //pick a "red" from 0 - 255
+    var r = Math.floor(Math.random() * 256);
+    //pick a "green" from 0 - 255
+    var g = Math.floor(Math.random() * 256);
+    //pick a "blue" from 0 - 255
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
